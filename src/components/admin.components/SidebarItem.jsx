e87df161e6e755bdbb42a05920b09ad8ee7380c0
mkkpkidangayam <mkkpkdm@gmail.com>
2024-08-03 16:37:37 +0530

    Updated

--- src/components/admin.components/SidebarItem.jsx
+++ src/components/admin.components/SidebarItem.jsx
@@ -1,7 +1,7 @@
 export const SidebarItem = ({ icon, label, isExpanded, onClick }) => {
   return (
     <div
-      className="flex items-center p-3 w-full hover:bg-gray-700 cursor-pointer"
+      className="flex items-center p-3 w-full hover:bg-cyan-800 cursor-pointer"
       onClick={onClick}
     >
       <div className="text-2xl">{icon}</div>
