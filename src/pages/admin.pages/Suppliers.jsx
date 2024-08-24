74a0dcef99f0b14261d85d48aee50e732e068bfb
Anshid <muhammedanshidh.t@gmail.com>
2024-08-24 10:45:05 +0530

    24-08-24

--- src/pages/admin.pages/Suppliers.jsx
+++ src/pages/admin.pages/Suppliers.jsx
@@ -1,32 +1,29 @@
-import React from 'react'
+import { useState } from 'react'
 import Cardpages from '../../components/admin.components/Cardpages'
 import TableComponent from '../../components/admin.components/TableComponent'
+import SupplierRegistration from '../../components/admin.components/Suppliers/SupplierRegistration'
+
+
 
 const Suppliers = () => {
 
+  const [open, setOpen] = useState(false)
+
   return (
     <div className='grid gap-3 p-3'>
-      <div className="flex  justify-end ">
-        <button className="bg-gray-700 text-white px-4 py-2 rounded-md flex items-center">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth="2"
-            stroke="currentColor"
-            className="w-10 h-5 mr-2"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M12 4v16m8-8H4"
-            />
-          </svg>
+      <div className="flex  justify-end relative" >
+        <button onClick={() => setOpen(true)} className="bg-gray-700 text-white px-4 py-2 rounded-md flex items-center">
           Add User
         </button>
       </div>
+      <SupplierRegistration
+        open={open}
+        onClose={() => setOpen(false)}
+      />
       <Cardpages />
       <TableComponent />
+
+
 
     </div >
   )
