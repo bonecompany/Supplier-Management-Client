260f4968bb6bb66d20917b82852a372fe4c8b0a9
Anshid <muhammedanshidh.t@gmail.com>
2024-10-22 15:36:03 +0530

    23 th commit for deployment branch for more responsive on more pages

--- src/components/UpdatingTableComponents.jsx
+++ src/components/UpdatingTableComponents.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from "react";
+import { useState } from "react";
 import { Axios } from "../MainRoute";
 import { toast } from "react-toastify";
 
@@ -7,95 +7,93 @@ function UpdatingTableComponents() {
     () => new Date().toISOString().split("T")[0]
   );
 
-
-  const [boneId, setBoneId] = useState("")
-  const [startDate, setStartDate] = useState("")
-  const [endDate, setEndDate] = useState("")
-  const [latexData, setLatexData] = useState([])
-  const [drcData, setDrcData] = useState([])
-  const [filWeight, setFilWeight] = useState(0)
-  const [drcPercentage, setDrcPercentage] = useState(0)
-  const [dryQuantity, setDryQuantity] = useState(0)
+  const [boneId, setBoneId] = useState("");
+  const [startDate, setStartDate] = useState("");
+  const [endDate, setEndDate] = useState("");
+  const [latexData, setLatexData] = useState([]);
+  const [drcData, setDrcData] = useState([]);
+  const [filWeight, setFilWeight] = useState(0);
+  const [drcPercentage, setDrcPercentage] = useState(0);
+  const [dryQuantity, setDryQuantity] = useState(0);
 
   const handleSearch = async () => {
     try {
-      const response = await Axios.get(`/admin/suppliers/latexdata?supplierId=${boneId}&start=${startDate}&end=${endDate}`);
-      setLatexData(response.data.data.latex)
-      setDrcData(latexData.map (({_id,owner,date,latex_weight}) =>  ({
-        latexId  : _id ,
-        owner,
-        date,
-        latex_weight
-      })))
-
+      const response = await Axios.get(
+        `/admin/suppliers/latexdata?supplierId=${boneId}&start=${startDate}&end=${endDate}`
+      );
+      setLatexData(response.data.data.latex);
+      setDrcData(
+        latexData.map(({ _id, owner, date, latex_weight }) => ({
+          latexId: _id,
+          owner,
+          date,
+          latex_weight,
+        }))
+      );
     } catch (err) {
       console.error("Error fetching latex data:", err);
     }
   };
 
-  
   const handleFilm = (e, index) => {
     const updatedValue = e.target.value;
-  
- 
+
     const updatedDrcData = drcData.map((item, ind) => {
       if (ind === index) {
         return {
-          ...item, 
-          filWeight: updatedValue,  
-          drcPercentage: updatedValue * 5,  
-          dryQuantity: item.latex_weight * (updatedValue * 5) / 100,  
+          ...item,
+          filWeight: updatedValue,
+          drcPercentage: updatedValue * 5,
+          dryQuantity: (item.latex_weight * updatedValue * 5) / 100,
         };
       }
       return item;
     });
-  
-    setDrcData(updatedDrcData);  
-    console.log(drcData)
+
+    setDrcData(updatedDrcData);
   };
 
-  const handleSubmit = async() => {
+  const handleSubmit = async () => {
     try {
-      const response = await Axios.get(`/admin//suppliers/drcupdation`,drcData);
-      setLatexData(response.data.data.latex)
-      setDrcData(latexData.map (({_id,owner,date,latex_weight}) =>  ({
-        latexId  : _id ,
-        owner,
-        date,
-        latex_weight
-      })))
-
+      const response = await Axios.get(`/admin/suppliers/drcupdation`, drcData);
+      setLatexData(response.data.data.latex);
+      setDrcData(
+        latexData.map(({ _id, owner, date, latex_weight }) => ({
+          latexId: _id,
+          owner,
+          date,
+          latex_weight,
+        }))
+      );
     } catch (err) {
       console.error("Error fetching latex data:", err);
     }
   };
-  
-
 
   return (
-    <div className="p-6 bg-[#F1F5F8] min-h-screen flex flex-col items-center">
-      <div className="bg-white shadow-lg rounded-lg p-4 w-full">
-        <div className="flex justify-between">
+    <div className="p-4 bg-[#F1F5F8] min-h-screen flex flex-col items-center">
+      <div className="bg-white shadow-lg rounded-lg p-4 w-full max-w-screen-lg">
+        <div className="flex flex-col lg:flex-row justify-between mb-4">
           <div>
-            <h2 className=" p-2 text-2xl font-bold text-gray-800 text-center">
-              Drc Updation Table
+            <h2 className="p-2 text-2xl font-bold text-gray-800 text-center lg:text-left">
+              DRC Updation Table
             </h2>
-            <div className="p-2 px-5 font-semibold text-lg">Name : {latexData[0]?.supplierName} </div>
+            <div className="p-2 px-5 font-semibold text-lg">
+              Name: {latexData[0]?.supplierName}
+            </div>
           </div>
-          <div className="bg-white rounded-lg px-4 mb-4 py-2 flex gap-4">
+          <div className="flex flex-wrap gap-4 bg-white rounded-lg px-4 py-2 items-end">
             <div>
               <label className="block text-gray-700">B-one ID</label>
               <input
-                className="p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-indigo-500 w-32"
+                className="p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-indigo-500 w-32 sm:w-40"
                 type="number"
                 placeholder="Enter Code"
                 onChange={(e) => setBoneId(e.target.value)}
               />
             </div>
             <div>
-            </div>
-            <div>
-             <label className="block text-gray-700">Start Date</label>
+              <label className="block text-gray-700">Start Date</label>
               <input
                 className="p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-indigo-500 w-40"
                 type="date"
@@ -110,73 +108,76 @@ function UpdatingTableComponents() {
                 onChange={(e) => setEndDate(e.target.value)}
               />
             </div>
+            <button
+              className="bg-slate-300 px-3 py-2 rounded-md font-semibold hover:bg-slate-700 hover:text-white duration-100"
+              onClick={handleSearch}
+            >
+              Search
+            </button>
           </div>
         </div>
-        <button className="float-right mb-8 mx-5 bg-slate-300 px-3 py-2 rounded-md font-semibold hover:bg-slate-700 hover:text-white duration-100"
-          onClick={handleSearch}>
 
-          Search
-        </button>
-        {/* Latex Input Table */}
-        <table className="table-auto w-full border-collapse border border-gray-300">
-          <thead className="bg-gray-200">
-            <tr>
-              <th className="border border-gray-300 p-3 text-center">#</th>
-              <th className="border border-gray-300 p-3 text-center">
-                Date
-              </th>
-              <th className="border border-gray-300 p-3 text-center">
-                Wet Weight
-              </th>
-              <th className="border border-gray-300 p-3 text-center">
-                Flm Weight
-              </th>
-              <th className="border border-gray-300 p-3 text-center">DRC Percentage </th>
-              <th className="border border-gray-300 p-3 text-center">Dry Quantity</th>
-              <th className="border border-gray-300 p-3 text-center">Serial No</th>
-              <th className="border border-gray-300 p-3 text-center">Mode</th>
-            </tr>
-          </thead>
-          <tbody>
-            {drcData?.map((element, index) => (
-              <tr className="bg-white hover:bg-gray-50 text-center" key={index}>
-                <td className="border border-gray-300 p-3">{index + 1}</td>
-                <td className="p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-indigo-500 w-28">
-
-                  {new Date(element.date).toLocaleDateString('en-GB')}
-
-                </td>
-                <td className="p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-indigo-500 w-24">
-                  {element.latex_weight}
-                </td>
-                <td className="border border-gray-300 p-3">
-                  <input
-                    className="p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-indigo-500 w-24"
-                    type="number"
-                    onChange={(e) => handleFilm(e, index)}
-                  />
-                </td>
-                <td className="p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-indigo-500 w-24">
-                  {element.drcPercentage || 0}
-                </td>
-                <td className="p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-indigo-500 w-24">
-                  {element.dryQuantity || 0}
-                </td>
-                <td className="p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-indigo-500 w-24">
-                  view
-                </td>
-                <td className="p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-indigo-500 w-24">
-                  Fied
-                </td>
+        {/* Responsive table */}
+        <div className="overflow-x-auto">
+          <table className="table-auto w-full border-collapse border border-gray-300">
+            <thead className="bg-gray-200">
+              <tr>
+                <th className="border border-gray-300 p-3 text-center">#</th>
+                <th className="border border-gray-300 p-3 text-center">Date</th>
+                <th className="border border-gray-300 p-3 text-center">
+                  Wet Weight
+                </th>
+                <th className="border border-gray-300 p-3 text-center">
+                  Flm Weight
+                </th>
+                <th className="border border-gray-300 p-3 text-center">
+                  DRC Percentage
+                </th>
+                <th className="border border-gray-300 p-3 text-center">
+                  Dry Quantity
+                </th>
+                <th className="border border-gray-300 p-3 text-center">
+                  Serial No
+                </th>
+                <th className="border border-gray-300 p-3 text-center">Mode</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {drcData?.map((element, index) => (
+                <tr
+                  className="bg-white hover:bg-gray-50 text-center"
+                  key={index}
+                >
+                  <td className="border border-gray-300 p-3">{index + 1}</td>
+                  <td className="border border-gray-300 p-3">
+                    {new Date(element.date).toLocaleDateString("en-GB")}
+                  </td>
+                  <td className="border border-gray-300 p-3">
+                    {element.latex_weight}
+                  </td>
+                  <td className="border border-gray-300 p-3">
+                    <input
+                      className="p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-indigo-500 w-full"
+                      type="number"
+                      onChange={(e) => handleFilm(e, index)}
+                    />
+                  </td>
+                  <td className="border border-gray-300 p-3">
+                    {element.drcPercentage || 0}
+                  </td>
+                  <td className="border border-gray-300 p-3">
+                    {element.dryQuantity || 0}
+                  </td>
+                  <td className="border border-gray-300 p-3">view</td>
+                  <td className="border border-gray-300 p-3">Filed</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
 
         <div className="flex justify-between px-2 mt-4">
-          <button
-            className="px-4 py-2 bg-gray-500 text-white rounded hover:bg-gray-600"
-          >
+          <button className="px-4 py-2 bg-gray-500 text-white rounded hover:bg-gray-600">
             Cancel
           </button>
           <button
@@ -189,6 +190,6 @@ function UpdatingTableComponents() {
       </div>
     </div>
   );
-};
+}
 
-export default UpdatingTableComponents
+export default UpdatingTableComponents;
--- src/components/admin.components/Suppliers/SupplierNavbar.jsx
+++ src/components/admin.components/Suppliers/SupplierNavbar.jsx
@@ -5,34 +5,32 @@ import SupplierRegistration from "./SupplierRegistration";
 
 function SupplierNavbar({ setSearchTerm, setSelectedCategory }) {
   const [open, setOpen] = useState(false);
+  
   return (
-    <div>
-      <div className="grid grid-cols-4 grid-rows-1 gap-2 bg-white p-2 rounded-md">
-        <div className="col-span-1">
-          <h1 className="text-2xl lg:text-3xl xl:text-4xl font-bold text-cyan-900">
-            Suppliers List
-          </h1>
-        </div>
-        <div className="col-span-3 flex justify-end gap-4">
-          <div className="grid grid-cols-3 gap-2">
-            <div className="col-span-2 flex px-4 py-3 grow rounded-md border-2 bg-white border-blue-200 overflow-hidden max-w-md  font-[sans-serif]">
+    <div className="p-4">
+      <div className="flex flex-col lg:flex-row lg:items-center lg:justify-between bg-white p-4 rounded-md shadow-md">
+        <h1 className="text-2xl lg:text-3xl xl:text-4xl font-bold text-cyan-900 mb-2 lg:mb-0">
+          Suppliers List
+        </h1>
+        
+        <div className="flex flex-col lg:flex-row lg:gap-4 w-full lg:w-auto">
+          <div className="flex flex-col lg:flex-row lg:gap-2 mb-2 lg:mb-0 w-full">
+            <div className="flex flex-grow border-2 border-blue-200 rounded-md overflow-hidden">
               <input
                 type="text"
                 placeholder="Search B one id or Supplier Name . . . "
-                className="w-full outline-none bg-transparent text-gray-600"
+                className="w-full p-2 outline-none bg-transparent text-gray-600"
                 onChange={(e) => setSearchTerm(e.target.value)}
               />
-              <CiSearch className=" text-2xl fill-gray-600" />
+              <CiSearch className="text-2xl fill-gray-600 p-2" />
             </div>
             <select
-              className="col-span-1 border rounded-md p-2"
+              className="border rounded-md p-2 mt-2 lg:mt-0"
               onChange={(e) => setSelectedCategory(e.target.value)}
             >
               <option value="All Suppliers">All Suppliers</option>
               <option value="Daily Collection">Daily Collection</option>
-              <option value="Alternative Day Collection">
-                Alternative Day Collection
-              </option>
+              <option value="Alternative Day Collection">Alternative Day Collection</option>
               <option value="Barrel Collection">Barrel Collection</option>
               <option value="Lease Plantation">Lease Plantation</option>
               <option value="Slaughter Plantation">Slaughter Plantation</option>
@@ -42,7 +40,7 @@ function SupplierNavbar({ setSearchTerm, setSelectedCategory }) {
             onClick={() => setOpen(true)}
             className="p-2 bg-cyan-800 text-white font-medium rounded-md flex items-center gap-1"
           >
-            <IoMdAdd className="w-10 h-5" />
+            <IoMdAdd className="w-6 h-6" />
             <span>Add Supplier</span>
           </button>
         </div>
